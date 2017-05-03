19ea00e51bce84d0265d538f1c964d1fe1b7c476
ukelulu <12228427@qq.com>
2017-05-03 10:09:37 +0800

    ch07 finished

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,8 +2,22 @@ class JobsController < ApplicationController
 before_action :authenticate_user! , only: [:new , :create , :edit , :update , :destroy]
 
   def index
-    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+    @jobs = case params[:order]
+    when 'by_lower_bound'
+      Job.published.order('waga_lower_bound DESC')
+    when 'by_upper_bound'
+      Job.published.order('waga_upper_bound DESC')
+    else
+      Job.published.recent
+    end
+
   end
+
+
+
+
+
+
   def new
     @job = Job.new
   end
